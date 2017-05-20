9da9af4e9ca48f300a9d3b9049ad610930d53aba
Vic <vrootic@gmail.com>
2017-05-20 16:44:40 -0700

    add log in user_detail.html

--- i2x_challenge/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ i2x_challenge/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -18,7 +18,7 @@ def update_site_forward(apps, schema_editor):
         id=settings.SITE_ID,
         defaults={
             'domain': 'example.com',
-            'name': 'Apex Usage Monitoring'
+            'name': 'i2x Challenge'
         }
     )
 
