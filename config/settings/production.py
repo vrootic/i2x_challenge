182e62b29b6473c4dc1355aefccd04d751ca8f6e
Vic <vrootic@gmail.com>
2017-05-20 13:46:45 -0700

    Add Sentry

--- config/settings/production.py
+++ config/settings/production.py
@@ -155,7 +155,7 @@ CACHES = {
 }
 
 
-Sentry Configuration
+# Sentry Configuration
 SENTRY_DSN = env('DJANGO_SENTRY_DSN')
 SENTRY_CLIENT = env('DJANGO_SENTRY_CLIENT', default='raven.contrib.django.raven_compat.DjangoClient')
 LOGGING = {
